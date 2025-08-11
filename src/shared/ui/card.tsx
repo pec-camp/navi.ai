e309694e78a885c4839a699154699b3c78eaaef9
BORA <a52447879@gmail.com>
2025-08-11 18:01:01 +0900

    fix: 버튼, 카드 컴포넌트 기본으로 롤백할게요.

--- src/shared/ui/card.tsx
+++ src/shared/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border border-input bg-card text-secondary",
+      "rounded-xl border bg-card text-card-foreground shadow",
       className,
     )}
     {...props}
