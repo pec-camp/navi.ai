28ddddd83e047b4a8837b8fc8898fbc51c62403b
Sumin Lee <48236404+nimuseel@users.noreply.github.com>
2025-08-15 20:53:05 +0900

    feat: 온보딩 UI 구현 (#2)

--- src/features/onboarding/index.ts
+++ src/features/onboarding/index.ts
@@ -1,4 +1,4 @@
-export { createUser } from "./api/createUser";
+export { saveOnboarding } from "./api/createUser";
 export { default as JobRoleSelector } from "./ui/JobRoleSelector";
 export { default as OnboardingSteps } from "./ui/OnboardingSteps";
 export { default as ToolSelector } from "./ui/ToolSelector";
