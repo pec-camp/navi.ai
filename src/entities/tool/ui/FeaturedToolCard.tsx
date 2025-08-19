6e69dd2189f86e363324230b011f87294a5815f0
BORA <a52447879@gmail.com>
2025-08-19 20:13:01 +0900

    feat: FeaturedToolCard 및 FeaturedToolList 데이터 바인딩

--- src/entities/tool/ui/FeaturedToolCard.tsx
+++ src/entities/tool/ui/FeaturedToolCard.tsx
@@ -1,52 +1,43 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/shared/ui/avatar";
 import { Card, CardContent } from "@/shared/ui/card";
 import { ArrowUpRight } from "lucide-react";
-
-interface FeaturedToolCardProps {
-  title: string;
-  description: string;
-  imageUrl: string;
-  tags: string[];
-  className?: string;
-}
+import { FeaturedTool } from "../model/FeaturedTool.interface";
+import ToolBadge from "./ToolBadge";
 
 export default function FeaturedToolCard({
-  title,
-  description,
-  imageUrl,
+  name,
+  websiteLogo,
+  whatIsSummary,
   tags,
+  slug,
+  isFree: isFree,
   className,
-}: FeaturedToolCardProps) {
+}: FeaturedTool & {
+  className?: string;
+}) {
   return (
     <Card
       className={`group relative transition-all duration-300 hover:-translate-y-1 hover:scale-[1.02] hover:shadow-sm ${className}`}
     >
       <CardContent className="space-y-4 p-6">
         <div className="flex items-center space-x-3">
-          <Avatar className="h-[34px] w-[34px] border border-border">
-            <AvatarImage src={imageUrl} alt={title} />
+          <Avatar className="h-[44px] w-[44px] border border-border">
+            <AvatarImage src={websiteLogo} alt={name} />
             <AvatarFallback className="bg-muted text-xs font-semibold text-foreground">
-              {title.charAt(0)}
+              {name.charAt(0)}
             </AvatarFallback>
           </Avatar>
           <h3 className="text-lg font-semibold leading-[27px] text-secondary">
-            {title}
+            {name}
           </h3>
         </div>
 
-        <div className="flex flex-wrap gap-2">
-          {tags.map((tag, index) => (
-            <span
-              key={index}
-              className="inline-flex items-center rounded-sm border border-border bg-transparent px-2 py-1 text-sm font-light text-muted-foreground-secondary shadow-sm"
-            >
-              {tag}
-            </span>
-          ))}
-        </div>
+        {/* 태그 */}
+        <ToolBadge tags={tags} />
 
+        {/* 요약 */}
         <p className="line-clamp-3 break-keep text-sm font-light leading-[19.6px] text-muted-foreground">
-          {description}
+          {whatIsSummary}
         </p>
       </CardContent>
 
