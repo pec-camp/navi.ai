fe48e53486c53e3aff506d662687e5d715b755a3
BORA <a52447879@gmail.com>
2025-08-27 01:37:54 +0900

    feat: FSD 폴더구조 재정리

--- src/features/search/index.ts
+++ src/features/search/index.ts
@@ -7,3 +7,5 @@ export type { UseSearchOptions, UseSearchReturn } from "./model";
 /** ui */
 export { MainSearchBar } from "./ui/MainSearchBar";
 export { ResultsBar } from "./ui/ResultsBar";
+export { SearchSuggestionItem } from "./ui/SearchSuggestionItem";
+export { ToolPalette } from "./ui/ToolPalette";
